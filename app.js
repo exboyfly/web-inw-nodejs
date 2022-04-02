89284e8a0dd7285820b516356c85f26c3a4c6c78
59160409 <59160409@go.buu.ac.th>
2022-04-02 22:24:38 +0700

    edit app.js file

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 
+let PORT = process.env.PORT || 5000;
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send("Hello World, NodeJS");
 });
 
-app.listen(3000, () => {
-    console.log("Port Listening to: 3000");
+app.listen(PORT, () => {
+    console.log("Port Listening to: " + PORT);
 });
 
