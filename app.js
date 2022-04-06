51a0fb47f049eafe7e5820dccf1f9dc758a848c7
59160409 <59160409@go.buu.ac.th>
2022-04-06 21:02:01 +0700

    add new file

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ const app = express();
 
 let PORT = process.env.PORT || 5000;
 
+// Static Files
 app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/img', express.static(__dirname + 'public/img'))
+app.use('/js', express.static(__dirname + 'public/js'))
 
-app.get('/', (req, res) => {
-    res.send("Hello World, NodeJS");
-});
+// Set Views
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.get('', (req, res) => {
+    
+    //res.send('hello')
+    res.render('index')
+
+})
 
 app.listen(PORT, () => {
-    console.log("Port Listening to: " + PORT);
-});
+    console.log("Port Listening to: " + PORT)
+})
 
